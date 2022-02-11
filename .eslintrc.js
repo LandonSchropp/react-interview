904d7e8f0b334e4d60997e34a2fa12906dd9ff0c
Landon Schropp <schroppl@gmail.com>
2022-02-10 20:05:23 -0600

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  extends: [ "@landonschropp", "@landonschropp/react" ],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true
+  }
+};
