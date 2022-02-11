b52ab5646b7c073582a940b70a33a232f91181b1
Landon Schropp <schroppl@gmail.com>
2022-02-10 20:10:11 -0600

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clearMocks: true,
+  testEnvironment: "jsdom",
+  "setupFilesAfterEnv": [ "<rootDir>/test/jest.setup.js" ]
+};
---
+++ test/jest.setup.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+
+import { configure } from "@testing-library/dom";
+
+// Monkey patch Jest so that tests without an implementation are marked as `todo`.
+const it = global.it;
+
+const wrappedIt = (description, func) => {
+  if (!func) {
+    return it.todo(description);
+  }
+
+  return it(description, func);
+};
+
+global.it = new Proxy(wrappedIt, {
+  get: (target, prop) => {
+    return it[prop];
+  }
+});
+
+// Configure the testing library.
+configure({ testIdAttribute: "data-test-id" });
